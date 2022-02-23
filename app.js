ed9f8996339700958523c5e5ea10686a365087ca
wanradavich <wrda.ruks@gmail.com>
2022-02-23 22:40:46 +0700

    Add pages and links

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 });
 
+app.get("/compose", function(req, res) {
+  res.render("compose")
+})
 
 
 
